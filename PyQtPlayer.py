4be11197139092a667806dd9e5813a116e2c3ee6
scavone87 <scavone87@gmail.com>
2023-01-01 15:07:35 +0100

    Added README.md and Requirements.txt

--- PyQtPlayer.py
+++ PyQtPlayer.py
@@ -10,7 +10,7 @@ import urllib.request
 
 import pandas as pd
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction, QGuiApplication
 from PyQt6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
                              QLineEdit, QMenu, QMenuBar, QMessageBox,
                              QProgressDialog, QPushButton, QTableWidget,
@@ -18,7 +18,8 @@ from PyQt6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
 
 import xtream
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class MainWindow(QWidget):
@@ -35,10 +36,18 @@ class MainWindow(QWidget):
 
     def initUI(self):
         logging.debug("INIT UI")
+        # Inizializza la finestra e posizionala al centro
+        self.setGeometry(100, 100, 900, 500)
+        self.setWindowTitle("Simple Xtreame Codes Player")
+        qr=self.frameGeometry()           
+        cp= QGuiApplication.primaryScreen().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
+        # Crea il menu bar
         menu_bar = QMenuBar()
 
-        # Crea il menu "Aiuto"
+        # Crea il menu "Aiuto
         help_menu = QMenu("Aiuto")
 
         # Crea l'azione "Informazioni su"
@@ -53,7 +62,6 @@ class MainWindow(QWidget):
         menu_bar.addMenu(help_menu)
 
         # Aggiungi la barra dei menu alla finestra principale
-       
 
         self.df_category = pd.DataFrame()
         self.option_category = xtream.liveType
@@ -64,24 +72,26 @@ class MainWindow(QWidget):
         # Crea la casella di input e il bottone
         self.input_box = QLineEdit(self)
         self.button = QPushButton("Clicca qui", self)
-        self.button.clicked.connect(self.onButtonClicked)  # Associa la funzione al bottone
+        # Associa la funzione al bottone
+        self.button.clicked.connect(self.onButtonClicked)
 
         # Crea i tre label
         self.label1 = QLabel("")
         self.label2 = QLabel("")
         self.label3 = QLabel("")
 
-        self.label1.setStyleSheet("color: green; font-weight: bold; font-family: Helvetica; font-size: 14px;")
-        self.label2.setStyleSheet("color: red; font-weight: bold; font-family: Helvetica; font-size: 14px;")
-        self.label3.setStyleSheet("color: blue; font-weight: bold; font-family: Helvetica; font-size: 14px;")
-
+        self.label1.setStyleSheet(
+            "color: green; font-weight: bold; font-family: Helvetica; font-size: 14px;")
+        self.label2.setStyleSheet(
+            "color: red; font-weight: bold; font-family: Helvetica; font-size: 14px;")
+        self.label3.setStyleSheet(
+            "color: blue; font-weight: bold; font-family: Helvetica; font-size: 14px;")
 
         # Crea la label per le opzioni
         self.options_label = QLabel("Scegli un'opzione")
 
         self.combo_box = QComboBox()
         self.combo_box.currentIndexChanged.connect(self.onDropdownChanged)
-
 
         # Aggiungi le opzioni alla casella di selezione
         self.combo_box.addItem(xtream.liveType)
@@ -93,12 +103,13 @@ class MainWindow(QWidget):
         self.search_box.textChanged.connect(self.onSearchChanged)
 
         self.search_box_channels = QLineEdit(self)
-        self.search_box_channels.textChanged.connect(self.onSearchChannelChanged)
+        self.search_box_channels.textChanged.connect(
+            self.onSearchChannelChanged)
 
         # Crea i due dataframe
-        self.table1 = QTableWidget(5,1)
-        self.table2 = QTableWidget(5,1)
-        self.table3 = QTableWidget(5,1)
+        self.table1 = QTableWidget(5, 1)
+        self.table2 = QTableWidget(5, 1)
+        self.table3 = QTableWidget(5, 1)
 
         self.table1.setEnabled(False)
         self.table2.setEnabled(False)
@@ -133,7 +144,6 @@ class MainWindow(QWidget):
         hbox.addWidget(self.search_box)
         hbox.addWidget(self.search_box_channels)
         vbox.addLayout(hbox)
-        
 
         # Imposta il layout orizzontale per i dataframe
         self.hbox = QHBoxLayout()
@@ -144,20 +154,19 @@ class MainWindow(QWidget):
         # Imposta il layout principale della finestra
         self.setLayout(vbox)
 
-
     def onAboutTriggered(self):
         message = """Questa è un'app PyQt6. È un player IPTV che sfrutta XTREAM CODES API per accedere alla lista dei canali. \nSfrutta VLC Media Player per la riproduzione (verifica di averlo installato correttamente). \nRicorda che Lo streaming di pay tv tramite IPTV è illegale. Questa applicazione implementa solo le API e non si assume alcuna resposabilità.
         \nRocco Scavone.
         """
         QMessageBox.about(self, "Informazioni sull'app", message)
 
-
     def onSearchChanged(self):
         # Ottieni il testo inserito nella casella di ricerca
         search_text = self.search_box.text().lower()
         # Scorri tutte le righe delle tabelle e nascondi quelle che non corrispondono alla ricerca
         for row in range(self.table1.rowCount()):
-            item = self.table1.item(row, 0)  # Prendi il primo elemento della riga (puoi scegliere qualsiasi altra colonna se vuoi cercare in un'altra colonna)
+            # Prendi il primo elemento della riga (puoi scegliere qualsiasi altra colonna se vuoi cercare in un'altra colonna)
+            item = self.table1.item(row, 0)
             if item is not None:
                 text = item.text().lower()
                 if search_text not in text:
@@ -170,7 +179,8 @@ class MainWindow(QWidget):
         search_text = self.search_box_channels.text().lower()
         # Scorri tutte le righe delle tabelle e nascondi quelle che non corrispondono alla ricerca
         for row in range(self.table2.rowCount()):
-            item = self.table2.item(row, 0)  # Prendi il primo elemento della riga (puoi scegliere qualsiasi altra colonna se vuoi cercare in un'altra colonna)
+            # Prendi il primo elemento della riga (puoi scegliere qualsiasi altra colonna se vuoi cercare in un'altra colonna)
+            item = self.table2.item(row, 0)
             if item is not None:
                 text = item.text().lower()
                 if search_text not in text:
@@ -182,19 +192,18 @@ class MainWindow(QWidget):
         # Prendi l'i-esimo elemento del dataframe
         self.progress_dialog.show()  # Mostra la ProgressDialog
         selected_item = self.df_category.iloc[row]
-        logging.debug("Categoria Selezionata: ",selected_item)
+        logging.debug("Categoria Selezionata: ", selected_item)
         category_id = selected_item['category_id']
-        r = xtream.streamsByCategory(self.option_category,category_id)
+        r = xtream.streamsByCategory(self.option_category, category_id)
         streams_by_category = r.json()
         self.df_channels = pd.DataFrame(streams_by_category)
         self.populateChannelsTable()
         self.progress_dialog.hide()
-        
 
     def onTable3CellDoubleClicked(self, row):
         self.progress_dialog.show()
         selected_item = self.df_series.iloc[row]
-        logging.debug("Episodio della serie selezionato: ",selected_item)
+        logging.debug("Episodio della serie selezionato: ", selected_item)
         self.url_channel = selected_item['url']
         self.playChannel()
         self.progress_dialog.hide()
@@ -203,21 +212,21 @@ class MainWindow(QWidget):
         # Prendi l'i-esimo elemento del dataframe
         self.progress_dialog.show()  # Mostra la ProgressDialog
         selected_item = self.df_channels.iloc[row]
-        logging.debug("Canale selezionato: ",selected_item)
+        logging.debug("Canale selezionato: ", selected_item)
         stream_id = selected_item.get("stream_id")
         series_id = selected_item.get("series_id")
         container_extension = selected_item.get("container_extension")
         if container_extension is not None and series_id is None:
             # È un film
             self.url_channel = f"{xtream.server}:8080/movie/{xtream.username}/{xtream.password}/{stream_id}.{container_extension}"
-            logging.debug("URL del film: ",self.url_channel)
+            logging.debug("URL del film: ", self.url_channel)
             self.progress_dialog.hide()
             self.playChannel()
 
         if stream_id is not None and container_extension is None:
             # È un live
             self.url_channel = f"{xtream.server}:8080/{xtream.username}/{xtream.password}/{stream_id}"
-            logging.debug("URL del canale live: ",self.url_channel)
+            logging.debug("URL del canale live: ", self.url_channel)
             self.progress_dialog.hide()
             self.playChannel()
         elif series_id is not None:
@@ -234,26 +243,25 @@ class MainWindow(QWidget):
         try:
             if platform.system() == 'Windows':
                 logging.info('Il sistema operativo è Windows')
-                subprocess.Popen(["C:/Program Files/VideoLAN/VLC/vlc.exe", self.url_channel])
+                subprocess.Popen(
+                    ["C:/Program Files/VideoLAN/VLC/vlc.exe", self.url_channel])
             elif platform.system() == 'Darwin':
                 logging.info('Il sistema operativo è Mac')
                 subprocess.Popen(["open", "-a", "vlc", self.url_channel])
             else:
-                logging.info('Il sistema operativo non è Windows o Mac')    
+                logging.info('Il sistema operativo non è Windows o Mac')
         except FileNotFoundError:
-            QMessageBox.warning(self,"Error", "VLC non è installato.")
+            QMessageBox.warning(self, "Error", "VLC non è installato.")
 
-        
     def clearContent(self):
         self.search_box.clear()
         self.search_box_channels.clear()
         self.table2.clear()
         self.table3.clear()
 
-
     def onDropdownChanged(self):
         self.clearContent()
-        
+
         if xtream.username:
             # Ottieni l'indice del valore selezionato
             selected_index = self.combo_box.currentIndex()
@@ -289,9 +297,11 @@ class MainWindow(QWidget):
         login_info = extract_login_info(input_text)
         if login_info[2]:
           server, username, password = login_info
-          logging.debug(f"Contenuto del login info: {server} - {username} - {password}")
+          logging.debug(
+              f"Contenuto del login info: {server} - {username} - {password}")
           # Scarica il contenuto dell'URL
-          self.progress_dialog = QProgressDialog("Caricamento in corso...", "Annulla", 0, 100, self)
+          self.progress_dialog = QProgressDialog(
+              "Caricamento in corso...", "Annulla", 0, 100, self)
           self.progress_dialog.setWindowModality(Qt.WindowModality.WindowModal)
           self.progress_dialog.setMinimumDuration(0)
           self.progress_dialog.show()  # Mostra la ProgressDialog
@@ -303,15 +313,17 @@ class MainWindow(QWidget):
           self.label1.setText(message)
           self.label2.setText(status)
           self.label3.setText(expire_date)
-          logging.debug(f"{message}, - Status: {status} - Data di scadenza: {expire_date}")
+          logging.debug(
+              f"{message}, - Status: {status} - Data di scadenza: {expire_date}")
           self.option_category = self.combo_box.currentText()
           catgegory_list = xtream.categories(self.option_category).json()
           self.df_category = pd.DataFrame(catgegory_list)
           self.populateCategoryTable()
-          self.progress_dialog.hide() 
+          self.progress_dialog.hide()
         else:
-            QMessageBox.warning(self, "Errore", "Il testo inserito non è un URL valido")
-            self.progress_dialog.hide() 
+            QMessageBox.warning(
+                self, "Errore", "Il testo inserito non è un URL valido")
+            self.progress_dialog.hide()
 
     def populateCategoryTable(self):
         self.table1.setEnabled(True)
@@ -320,7 +332,8 @@ class MainWindow(QWidget):
         self.table1.setColumnCount(1)
         # Inserisci i dati del dataframe nella tabella
         for i in range(self.df_category.shape[0]):
-            self.table1.setItem(i-1,1,QTableWidgetItem(str(self.df_category.iloc[i,1])))
+            self.table1.setItem(
+                i-1, 1, QTableWidgetItem(str(self.df_category.iloc[i, 1])))
 
     def populateChannelsTable(self):
         # Imposta il numero di righe e colonne della tabella
@@ -329,7 +342,8 @@ class MainWindow(QWidget):
         self.table2.setColumnCount(1)
         # Inserisci i dati del dataframe nella tabella
         for i in range(self.df_channels.shape[0]):
-            self.table2.setItem(i-1,1,QTableWidgetItem(str(self.df_channels.iloc[i,1])))
+            self.table2.setItem(
+                i-1, 1, QTableWidgetItem(str(self.df_channels.iloc[i, 1])))
 
     def populateSeriesEpisodeTable(self):
         # Imposta il numero di righe e colonne della tabella
@@ -338,20 +352,10 @@ class MainWindow(QWidget):
         self.table3.setColumnCount(1)
         # Inserisci i dati del dataframe nella tabella
         for i in range(self.df_series.shape[0]):
-            self.table3.setItem(i-1,1,QTableWidgetItem(str(self.df_series.iloc[i,0])))
+            self.table3.setItem(
+                i-1, 1, QTableWidgetItem(str(self.df_series.iloc[i, 0])))
 
 
-"""def creaDataframeSeries(series):
-    row = []
-    for season in series["episodes"]:
-        for episode in series["episodes"][season]:
-            title = episode["title"]
-            url = f"{xtream.server}/series/{xtream.username}/{xtream.password}/{episode['id']}.{episode['container_extension']}"
-            row.append((title,url))
-    df = pd.DataFrame(row, columns=["title","url"])
-    return df"""
-    
-    
 def creaDataframeSeries(series):
     row = []
     with multiprocessing.Pool() as pool:
@@ -359,10 +363,9 @@ def creaDataframeSeries(series):
             for episode in series["episodes"][season]:
                 title = episode["title"]
                 url = f"{xtream.server}/series/{xtream.username}/{xtream.password}/{episode['id']}.{episode['container_extension']}"
-                row.append((title,url))
-    df = pd.DataFrame(row, columns=["title","url"])
+                row.append((title, url))
+    df = pd.DataFrame(row, columns=["title", "url"])
     return df
-
 
 
 def extract_login_info(url):
@@ -383,6 +386,7 @@ def get_category_type(xtream_type):
     category = xtream.categories(xtream_type)
     return pd.DataFrame(category.json())
 
+
 def xtream_auth(server_name, username, password):
     xtream.server = f"http://{server_name}"
     xtream.username = username
@@ -391,11 +395,8 @@ def xtream_auth(server_name, username, password):
     return r.json()
 
 
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
-
---
+++ README.md
@@ -0,0 +1,33 @@
+# Simple Xtreame Codes Player
+
+Questa è una semplice applicazione che ti permette di riprodurre i contenuti di Xtreame Codes.
+
+**Nota: questa applicazione richiede l'installazione di VLC Media Player.**
+
+## Requisiti
+
+Per utilizzare questa applicazione, hai bisogno di:
+
+- Python 3.5 o successivo
+- PyQt6
+- Pandas
+- VLC Media Player
+
+## Installazione
+
+Per installare questa applicazione, segui questi passi:
+
+- Assicurati di avere i requisiti sopra elencati installati sul tuo sistema, incluso VLC Media Player.
+- Scarica o clona il repository dal seguente link: https://github.com/scavone87/Simple-Xtreame-Codes-Player
+Apri una finestra del terminale e naviga alla cartella del repository.
+- Esegui il comando pip install -r requirements.txt per installare le dipendenze necessarie.
+
+## Utilizzo
+
+Per utilizzare questa applicazione, segui questi passi:
+
+-  Apri una finestra del terminale e naviga alla cartella del repository.
+- Esegui il comando python main.py per avviare l'applicazione.
+- Seleziona il tipo di contenuti che vuoi visualizzare (Live, VOD o Serie TV) dal menu a tendina.
+Utilizza la casella di ricerca per filtrare i contenuti disponibili.
+- Fai clic su uno dei contenuti nella tabella per avviarne la riproduzione.
